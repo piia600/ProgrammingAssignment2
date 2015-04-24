db2ae34f94a1def55969ccb8d0d7c9a2ba2648c9
piia600 <piia600@gmail.com>
2015-04-24 12:11:02 +0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x=matrix, ...) {
 
+	  cache <- NULL
+        setmatrix <- function(y) {
+                x <<- y
+                cache <<- NULL
+        }
+        getmatrix <- function() x <- matrix()
+        setinverse <- function(solve) cache <<- solve(x)
+        getinverse <- function() cache
+        list(setmatrix = setmatrix, getmatrix = getmatrix,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+##  This function computes the inverse of the special "matrix" returned 
+##  by makeCacheMatrix above. If the inverse has already been calculated 
+##  (and the matrix has not changed), then cacheSolve should retrieve the 
+##  inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(matrix, ...) {
+        ## Check for a matrix that is the inverse of 'x'
+    matrix <- matrix$getmatrix()
+    
+    if(!is.null(cache)){
+      return(cache)
+    }
+
+    cache <- matrix$setinverse()
+    matrix$setinverse(cache)
+    return(cache)
 }
+
+## This gives an error about atomic vectors but I don't have time to debug the code. :)
